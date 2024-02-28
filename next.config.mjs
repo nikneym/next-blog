2f1e083d3d7622f80a7483d378672aa8fd5ecb72
nikneym <halildrk@gmail.com>
2024-02-28 17:29:45 +0300

    Setup image configurations since none are loading

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 300],
+    // path prefix for Image Optimization API, useful with `loader`
+    path: "/_next/image",
+    // loader can be 'default', 'imgix', 'cloudinary', 'akamai', or 'custom'
+    loader: "default",
   },
   output: "export",
 };
